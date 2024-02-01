f56cc4db24773b1a24aeef812f91aa88b65d261d
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-02-01 18:25:41 +0200

    added interceptor global & forget-pass integration

--- src/app/core/auth/components/forget-pass/forget-pass.component.ts
+++ src/app/core/auth/components/forget-pass/forget-pass.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-forget-pass',
   templateUrl: './forget-pass.component.html',
-  styleUrls: ['./forget-pass.component.scss']
+  styleUrls: ['./forget-pass.component.scss'],
 })
 export class ForgetPassComponent {
+  constructor(
+    private _authService: AuthService,
+    private _ToastrService: ToastrService,
+    private _router: Router
+  ) {}
+  email: string = '';
 
+  onSubmit(email: string) {
+    this._authService.onForgetPassword(email).subscribe({
+      next: (res) => {
+        console.log(res)
+      },
+      error: (err) => {
+        this._ToastrService.error(err.error.errorMessage, 'Error!');
+      },
+      complete: () => {
+        this._ToastrService.success("Request Success", 'Successfully!');
+        this._router.navigate(['/auth/resetPassword']);
+        localStorage.setItem('email' , email);
+      },
+    });
+  }
 }
