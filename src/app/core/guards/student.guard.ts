5cdf82acedd372a313f671708aff0f3316e560ca
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-02-03 16:47:30 +0200

    added heriarchy-arch for instructor module

--- src/app/core/guards/student.guard.ts
+++ src/app/core/guards/student.guard.ts
@@ -1,10 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
 
 export const studentGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const authService= inject(AuthService);
   if (localStorage.getItem('userToken') !== null && localStorage.getItem('role') == 'user') {
     return true;
 
   } else {
+    // authService.getRole();
+    // router.navigate(['/dashboard']);
     return false
   }
 };
