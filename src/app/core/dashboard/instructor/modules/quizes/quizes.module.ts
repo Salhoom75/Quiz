bbdd68892cb007e1355e64abf8d6a5e27ee73b73
Salhoom75 <salhoomadel@gmail.com>
2024-02-04 19:00:27 +0200

    quiz-creation ui

--- src/app/core/dashboard/instructor/modules/quizes/quizes.module.ts
+++ src/app/core/dashboard/instructor/modules/quizes/quizes.module.ts
@@ -4,12 +4,14 @@ import { QuizesRoutingModule } from './quizes-routing.module';
 import { QuizesComponent } from './components/quizes/quizes.component';
 import { SetUpQuizComponent } from './components/set-up-quiz/set-up-quiz.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { QuizCreatedComponent } from './components/set-up-quiz/components/quiz-created/quiz-created.component';
 
 
 @NgModule({
   declarations: [
     QuizesComponent,
-    SetUpQuizComponent
+    SetUpQuizComponent,
+    QuizCreatedComponent
   ],
   imports: [
     CommonModule,
