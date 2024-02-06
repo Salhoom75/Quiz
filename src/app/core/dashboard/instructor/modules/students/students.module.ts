24319bf6a47e869cbbbead3792a6738d8706e164
EmanMohamed22 <emyelgarf22@gmail.com>
2024-02-07 00:35:47 +0200

    view-result&add-student

--- src/app/core/dashboard/instructor/modules/students/students.module.ts
+++ src/app/core/dashboard/instructor/modules/students/students.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { StudentsRoutingModule } from './students-routing.module';
 import { StudentsComponent } from './components/students/students.component';
+import { AddUpdateStudentComponent } from './components/add-update-student/add-update-student.component';
 
 
 @NgModule({
   declarations: [
-    StudentsComponent
+    StudentsComponent,
+    AddUpdateStudentComponent
   ],
   imports: [
     CommonModule,
