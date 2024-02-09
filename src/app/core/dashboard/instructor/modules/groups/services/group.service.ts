d5657d137de1fcf783c35c384cf22737d6bd3cc0
Ahmed-Adel33 <ahmed920adel@gmail.com>
2024-02-09 19:41:44 +0200

    add group

--- src/app/core/dashboard/instructor/modules/groups/services/group.service.ts
+++ src/app/core/dashboard/instructor/modules/groups/services/group.service.ts
@@ -1,16 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { addGroup } from '../models/group';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GroupService {
+  constructor(private _HttpClient: HttpClient) {}
 
-  constructor(private _HttpClient:HttpClient) { }
-
-
-  getAllGroups():Observable<any>{
-    return this._HttpClient.get('group')
+  getAllGroups(): Observable<any> {
+    return this._HttpClient.get('group');
+  }
+  createGroups(data: addGroup): Observable<any> {
+    return this._HttpClient.post('group', data);
   }
 }
