cd24a25cbc618d086572378cf30fddcd2ed2f52f
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-02-14 19:46:00 +0200

    added add, update & delete from group endpoints

--- src/app/core/dashboard/instructor/modules/students/services/students.service.ts
+++ src/app/core/dashboard/instructor/modules/students/services/students.service.ts
@@ -7,17 +7,26 @@ import { Observable } from 'rxjs';
 })
 export class StudentsService {
   constructor(private _HttpClient: HttpClient) {}
- 
+
   getAllStudents(): Observable<any> {
     return this._HttpClient.get('student');
   }
-  deleteStudent(id:string): Observable<any> {
+  deleteStudent(id: string): Observable<any> {
     return this._HttpClient.delete(`student/${id}`);
   }
-  getStudentById(id:string): Observable<any> {
+  getStudentById(id: string): Observable<any> {
     return this._HttpClient.get(`student/${id}`);
   }
   getAllStudentsWithoutGroup(): Observable<any> {
     return this._HttpClient.get('student/without-group');
+  }
+  deleteStudentFromGroup(studentId: string, groupId: string): Observable<any> {
+    return this._HttpClient.delete(`student/${studentId}/${groupId}`);
+  }
+  addStudentToGroup(studentId: string, groupId: string): Observable<any> {
+    return this._HttpClient.get(`student/${studentId}/${groupId}`);
+  }
+  updateStudentToGroup(studentId: string, groupId: string): Observable<any> {
+    return this._HttpClient.put(`student/${studentId}/${groupId}`,{});
   }
 }
