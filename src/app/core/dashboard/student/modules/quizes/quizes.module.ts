8f0fc0cb647e4054b62f2d3aa2596fa3777b313e
Ahmed-Adel33 <ahmed920adel@gmail.com>
2024-02-15 01:40:37 +0200

    quizes student

--- src/app/core/dashboard/student/modules/quizes/quizes.module.ts
+++ src/app/core/dashboard/student/modules/quizes/quizes.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { QuizesRoutingModule } from './quizes-routing.module';
 import { QuizesComponent } from './components/quizes/quizes.component';
-
+import { SharedModule } from '../../../../../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    QuizesComponent
-  ],
-  imports: [
-    CommonModule,
-    QuizesRoutingModule
-  ]
+  declarations: [QuizesComponent],
+  imports: [CommonModule, QuizesRoutingModule, SharedModule],
 })
-export class QuizesModule { }
+export class QuizesModule {}
