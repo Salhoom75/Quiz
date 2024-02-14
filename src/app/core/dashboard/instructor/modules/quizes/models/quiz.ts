e492307808475e58f11947335716d3a7ccf34c78
EmanMohamed22 <emyelgarf22@gmail.com>
2024-02-14 23:48:48 +0200

    Create Quiz integration

--- src/app/core/dashboard/instructor/modules/quizes/models/quiz.ts
+++ src/app/core/dashboard/instructor/modules/quizes/models/quiz.ts
@@ -1,2 +1,11 @@
 export interface Quiz {
+    title:string,
+    description:string,
+    group:string,
+    questions_number:number,
+    difficulty:string,
+    type:string,
+    schadule:string,
+    duration:number,
+    score_per_question:number
 }
--- src/app/core/dashboard/instructor/modules/quizes/services/quizes.service.ts
+++ src/app/core/dashboard/instructor/modules/quizes/services/quizes.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Quiz } from '../models/quiz';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizesService {
 
-  constructor() { }
+  constructor(private _HttpClient:HttpClient) { }
+
+  createQuiz(data: Quiz): Observable<any> {
+    return this._HttpClient.post('quiz', data);
+  }
 }
