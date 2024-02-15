c1593f45bc330f3e86e5200556a179ed5ef87d85
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2024-02-15 17:04:31 +0200

    enahncement arch quizes modulue

--- src/app/core/dashboard/instructor/modules/quizes/components/quiz-details/quiz-details.component.ts
+++ src/app/core/dashboard/instructor/modules/quizes/components/quiz-details/quiz-details.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { QuizesService } from '../quizes/services/quizes.service';
 @Component({
   selector: 'app-quiz-details',
   templateUrl: './quiz-details.component.html',
