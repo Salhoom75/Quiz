c5d43dd41dabb1367a56b5114c29b26ca62073b9
EmanMohamed22 <emyelgarf22@gmail.com>
2024-02-16 02:12:09 +0200

    incoming 5 quizzes

--- src/app/core/dashboard/student/modules/quizes/components/quizes/quizes.component.ts
+++ src/app/core/dashboard/student/modules/quizes/components/quizes/quizes.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizesService } from 'src/app/core/dashboard/instructor/modules/quizes/services/quizes.service';
+import { QuizeStudentService } from '../../services/quize-student.service';
 
 @Component({
   selector: 'app-quizes',
   templateUrl: './quizes.component.html',
   styleUrls: ['./quizes.component.scss']
 })
-export class QuizesComponent {
+export class QuizesComponent implements OnInit{
+  quizzes:any[]=[]
+constructor(private _QuizService:QuizeStudentService){}
 
+ngOnInit(): void {
+  this.getIncoming5Quizzes()
+}
+getIncoming5Quizzes(){
+this._QuizService.getFirst5IncomingQuizzes().subscribe({
+  next:(res)=>{
+    console.log(res);
+    this.quizzes=res
+  }
+})
+}
 }
