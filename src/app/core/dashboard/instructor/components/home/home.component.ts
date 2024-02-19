b1b46e53724cc30668a5272ad4e4f32aced4b559
Ahmed-Adel33 <ahmed920adel@gmail.com>
2024-02-20 01:17:39 +0200

    Upcoming quizess home

--- src/app/core/dashboard/instructor/components/home/home.component.ts
+++ src/app/core/dashboard/instructor/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { InstructorService } from '../../services/instructor.service';
 import { Student } from '../../modules/students/models/student';
+import { QuizesService } from '../../modules/quizes/services/quizes.service';
 
 @Component({
   selector: 'app-home',
@@ -9,21 +10,35 @@ import { Student } from '../../modules/students/models/student';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  IncomingStudents: any;
   userId: string;
-  topStudents:Student[]=[]
-  constructor(private _ActivatedRoute: ActivatedRoute,private _InstructorService:InstructorService) {
+  topStudents: Student[] = [];
+  constructor(
+    private _ActivatedRoute: ActivatedRoute,
+    private _InstructorService: InstructorService,
+    private _QuizesService: QuizesService
+  ) {
     this.userId = _ActivatedRoute.snapshot.params['id'];
   }
 
   ngOnInit(): void {
-    this.getTopStudents()
+    this.getTopStudents();
+    this.getFiveIncommingStudents();
   }
-  getTopStudents(){
+  getTopStudents() {
     this._InstructorService.getTopFiveStudents().subscribe({
-      next:(res)=>{
+      next: (res) => {
         console.log(res);
-        this.topStudents=res
-      }
-    })
+        this.topStudents = res;
+      },
+    });
+  }
+  getFiveIncommingStudents() {
+    this._QuizesService.getFiveIncommingStudents().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.IncomingStudents = res;
+      },
+    });
   }
 }
