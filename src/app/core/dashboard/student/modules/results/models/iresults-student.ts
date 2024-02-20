0c4bec12210192a43cdbd70f800768e3b131b965
EmanMohamed22 <emyelgarf22@gmail.com>
2024-02-21 01:21:04 +0200

    student results ui and integration

--- src/app/core/dashboard/student/modules/results/models/iresults-student.ts
+++ src/app/core/dashboard/student/modules/results/models/iresults-student.ts
@@ -1,2 +1,12 @@
+import { Quiz } from "src/app/core/dashboard/instructor/modules/quizes/models/quiz";
+import { Iparticipant } from "src/app/core/dashboard/instructor/modules/results/models/results";
+
 export interface IResultsStudent {
+    quiz:Quiz,
+    result:IResult
+}
+export interface IResult {
+    quiz:Quiz,
+    score:number,
+    participant:Iparticipant
 }
